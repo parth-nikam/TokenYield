18dd4dcf97cba5c819b09e63abe73c22022f204d
Parth Nikam <parthsnikam12co@gmail.com>
2024-07-20 23:44:56 +0530

    Correcting errors

--- src/TokenYield_assets/src/index.js
+++ src/TokenYield_assets/src/index.js
@@ -1,39 +1,39 @@
 import { TokenYield } from "../../declarations/TokenYield";
 
 window.addEventListener("load", async function() {
-    update();
+  update();
 });
 
 document.querySelector("form").addEventListener("submit", async function(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const button = event.target.querySelector("#submit-btn");
+  const button = event.target.querySelector("#submit-btn");
 
-    const inputAmount = parseFloat(document.getElementById("input-amount").value);
-    const outputAmount = parseFloat(document.getElementById("withdraw-amount").value);
+  const inputAmount = parseFloat(document.getElementById("input-amount").value);
+  const outputAmount = parseFloat(document.getElementById("withdraw-amount").value);
 
-    button.setAttribute("disabled", true);
+  button.setAttribute("disabled", true);
 
-    if (document.getElementById("input-amount").value.length != 0) {
-        await TokenYield.topUp(inputAmount);
-      }
+  if (document.getElementById("input-amount").value.length != 0) {
+    await TokenYield.topUp(inputAmount);
+  }
+  
+  if (document.getElementById("withdraw-amount").value.length != 0) {
+    await TokenYield.withdraw(outputAmount);
+  }
+  
+  await TokenYield.compound();
+
+  update()
+  
+  document.getElementById("withdraw-amount").value = "";
+  document.getElementById("input-amount").value = "";
+
+  button.removeAttribute("disabled");
+});
     
-      if (document.getElementById("withdraw-amount").value.length != 0) {
-        await TokenYield.withdraw(outputAmount);
-      }
+async function update() {
+  const currentAmount = await TokenYield.checkBalance();
+  document.getElementById("value").innerText = Math.round(currentAmount * 100) / 100;
+};
     
-      await TokenYield.compound();
-    
-      update()
-    
-      document.getElementById("input-amount").value = "";
-      document.getElementById("withdraw-amount").value = "";
-    
-      button.removeAttribute("disabled");
-    
-    });
-    
-    async function update() {
-      const currentAmount = await TokenYield.checkBalance();
-      document.getElementById("value").innerText = Math.round(currentAmount * 100) / 100;
-    };
